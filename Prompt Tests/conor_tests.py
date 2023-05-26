1956949d34d8ff1c8a526f16a58fc1e3479e5144
Conor Bailey <cpbailey025@gmail.com>
2023-05-26 23:00:18 +0100

    made it similar to that online

--- Prompt Tests/conor_tests.py
+++ Prompt Tests/conor_tests.py
@@ -2,20 +2,26 @@ import openai
 
 openai.api_key = 'Your API Key'
 
-prompt = "You are a intelligent assistant."
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.75,
+    )
+    return response.choices[0].message["content"]
 
-messages = [ { "role": "system", "content": prompt} ]
+sentiment = "Therapist"
 
-while True:
-    message = input("User: ")
-    if message:
-        messages.append(
-            {"role": "user", "content": message}
-        )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages = messages
-            )
-    
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": "reply"})
+user_sheet = """
+I am sad and lonley!
+"""
+
+prompt = f"""
+YOur role is to act as a {sentiment} to the user
+provide valuable advice based on the users mood
+as defined in the fact sheet
+
+User fact sheet: ```{user_sheet}```
+"""
+response = get_completion(prompt)
