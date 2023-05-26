3ea5a357e6789009bde97efc3f16488ba40d7445
Conor Bailey <cpbailey025@gmail.com>
2023-05-26 22:18:32 +0100

    added the methods needed to make a bot

--- Prompt Tests/conor_tests.py
+++ Prompt Tests/conor_tests.py
@@ -1,1 +1,21 @@
-print("lets gooo")
+import openai
+
+openai.api_key = 'Your API Key'
+
+prompt = "You are a intelligent assistant."
+
+messages = [ { "role": "system", "content": prompt} ]
+
+while True:
+    message = input("User: ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message}
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages = messages
+            )
+    
+    reply = chat.choices[0].message.content
+    print(f"ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": "reply"})
